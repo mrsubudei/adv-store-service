04d72e1b6824d68045d14ad6d6d21fcb45ff2b64
Meiram <94844462+mrsubudei@users.noreply.github.com>
2022-12-28 15:44:03 +0600

    Update errors.go

--- internal/entity/errors.go
+++ internal/entity/errors.go
@@ -5,7 +5,7 @@ import "errors"
 var (
 	ErrNameAlreadyExist = errors.New("name already exists")
 	ErrItemNotExists    = errors.New("item not exists")
-	ErrItemsNotExist    = errors.New("there are no items")
+	ErrNoItems          = errors.New("there are no items")
 	ErrUpdateFailed     = errors.New("update failed")
 	ErrDeleteFailed     = errors.New("delete failed")
 )
