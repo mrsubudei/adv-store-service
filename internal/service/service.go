97cfb7edc1ea50ba149b5bb45e9d5f78fc58b0c0
Meiram <94844462+mrsubudei@users.noreply.github.com>
2022-12-28 17:08:14 +0600

    Create service.go

---
+++ internal/service/service.go
@@ -0,0 +1,14 @@
+package service
+
+import (
+  "context"
+  "github.com/mrsubudei/adv-store-service/internal/entity"
+)
+
+type Service interface {
+  Create(ctx context.Context, adv entity.Advert) error
+  GetById(ctx context.Context, id int64) (entity.Advert, error)
+  GetAll(ctx context.Context) ([]entity.Advert, error)
+  Update(ctx context.Context, adv entity.Advert) error
+  Delete(ctx context.Context, id int64) error
+}
