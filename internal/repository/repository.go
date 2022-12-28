8742ae0886a078dc257a031928e630469b916670
Meiram <94844462+mrsubudei@users.noreply.github.com>
2022-12-28 11:22:48 +0600

    Update repository.go

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,3 +1,10 @@
 package repository
 
-type Advert interface{}
+type Advert interface{
+  Store(ctx context.Context, adv *entity.Advert) error
+  GetById(ctx context.Context, id int64) (entity.Advert, error)
+  Fetch(ctx context.Context) ([]entity.Advert, error)
+  Update(ctx context.Context, adv entity.Advert) error
+  Delete(ctx context.Context, id int64) error
+  DeleteUrl(ctx context.Context, url string) error
+}
