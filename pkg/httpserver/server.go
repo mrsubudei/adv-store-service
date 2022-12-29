d6611d36e3f9913ea46ad828d5bba3f53643228d
Meiram <94844462+mrsubudei@users.noreply.github.com>
2022-12-29 17:37:53 +0600

    Update server.go

--- pkg/httpserver/server.go
+++ pkg/httpserver/server.go
@@ -1,40 +1,41 @@
 package httpserver
 
 import (
-	"context"
-	"net/http"
-	"time"
+        "context"
+        "net/http"
+        "time"
 
-	v1 "github.com/mrsubudei/adv-store-service/internal/controller/http/v1"
+        v1 "github.com/mrsubudei/adv-store-service/internal/controller/http/v1"
 )
 
 type Server struct {
-	httpServer *http.Server
-	h          *v1.Handler
+        httpServer *http.Server
+        h          *v1.Handler
 }
 
 const DefaultTime = int(time.Second)
 
 func NewServer(handler *v1.Handler) *Server {
-	return &Server{
-		httpServer: &http.Server{
-			Addr:         handler.Cfg.Server.Port,
-			ReadTimeout:  time.Duration(handler.Cfg.Server.ReadTimeout * DefaultTime),
-			WriteTimeout: time.Duration(handler.Cfg.Server.WriteTimeout * DefaultTime),
-			Handler:      handler.Mux,
-		},
-		h: handler,
-	}
+        return &Server{
+                httpServer: &http.Server{
+                        Addr:         handler.Cfg.Server.Port,
+                        ReadTimeout:  time.Duration(handler.Cfg.Server.ReadTimeout * DefaultTime),
+                        WriteTimeout: time.Duration(handler.Cfg.Server.WriteTimeout * DefaultTime),
+                        Handler:      handler.Mux,
+                },
+                h: handler,
+        }
 }
 
 func (s *Server) Run() error {
-	s.h.Mux.HandleFunc("/v1/", s.h.NewGroup)
-	return s.httpServer.ListenAndServe()
+        s.h.Mux.HandleFunc("/v1/adverts", s.h.NewCommonRoutes)
+        s.h.Mux.HandleFunc("/v1/adverts/", s.h.NewParticularRoutes)
+        return s.httpServer.ListenAndServe()
 }
 
 func (s *Server) Shutdown() error {
-	ctx, cancel := context.WithTimeout(context.Background(),
-		time.Duration(s.h.Cfg.Server.ShutDownTimeout*DefaultTime))
-	defer cancel()
-	return s.httpServer.Shutdown(ctx)
+        ctx, cancel := context.WithTimeout(context.Background(),
+                time.Duration(s.h.Cfg.Server.ShutDownTimeout*DefaultTime))
+        defer cancel()
+        return s.httpServer.Shutdown(ctx)
 }
